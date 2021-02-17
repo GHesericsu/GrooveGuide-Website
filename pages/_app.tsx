79d3ac4cd6b09f79d2d08652cb4b40f2b09655ef
Eric Su <esericsu@gmail.com>
2021-02-17 14:47:09 -0800

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import withReduxStore from '../lib/with-redux-store';
+import { Provider } from 'react-redux';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+const GlobalStyle = createGlobalStyle`
+ * {
+    box-sizing: border-box;
+  }
+  html {
+    height: 100%;
+    width: auto;
+    background: #070606;
+    font-family: 'Nunito', sans-serif;
+  }
+`;
+
+interface AppPropsWithRedux extends AppProps {
+  reduxStore: any;
+}
+
+function MyApp({ Component, pageProps, reduxStore }: AppPropsWithRedux) {
+  return (
+    <Provider store={reduxStore}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </Provider>
+  );
+}
+
+export default withReduxStore(MyApp);
---
+++ src/components/Layout.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+
+const Layout = ({ children, home }) => {
+  return (
+    <main>{children}</main>
+  )
+}
